ea26e3552c9cfe4d21ad13ee0247a6fa8605ad13
Hiroko Sakai <hiroko.hirobo@gmail.com>
2023-04-06 09:40:31 +0200

    add new columns, update regex for extract region from place

--- dbt/models/core/fact_world_earthquake.sql
+++ dbt/models/core/fact_world_earthquake.sql
@@ -1,23 +1,55 @@
 {{ config(materialized='table') }}
 
+
 SELECT
+  id,
   time,
-  EXTRACT(YEAR from time) as year,
-  EXTRACT(MONTH from time) as month,
-  EXTRACT(DAYOFWEEK from time) as day_of_week,
+  EXTRACT(YEAR
+  FROM
+    time) AS year,
+  EXTRACT(MONTH
+  FROM
+    time) AS month,
+  EXTRACT(DAYOFWEEK
+  FROM
+    time) AS day_of_week,
   place,
   type,
   latitude,
   longitude,
+  depth,
+  mag,
+  magType,
   CASE
-    WHEN CONTAINS_SUBSTR(place, ",") THEN REGEXP_EXTRACT(place, r',\s(.*?)$',1)
-    WHEN NOT CONTAINS_SUBSTR(place, ",") THEN REGEXP_EXTRACT(place, r'\s([\w]*)$',1)
+    WHEN REGEXP_CONTAINS(region, r'of (?:the )?(\w+ ?\w+) island') THEN REGEXP_EXTRACT(region, r'of (?:the )?(\w+ ?\w+) island')
+    WHEN REGEXP_CONTAINS(region, r'(off|near)?\s?(the)?\s?\b(bay|coast|central|south|north|east|west|southeast|southwest|northwest|northeast|southeastern)( coast)? of\s?\b(south|north|west|east|southern|northern|western|eastern|central|southeastern)?\s?') THEN REGEXP_REPLACE(region, r'(off|near)?\s?(the)?\s?\b(bay|coast|central|south|north|east|west|southeast|southwest|northwest|northeast|southeastern)( coast)? of\s?\b(south|north|west|east|southern|northern|western|eastern|central|southeastern)?\s?', '')
+    WHEN REGEXP_CONTAINS(region, r'sea of (\w+)') THEN REGEXP_EXTRACT(region, r'sea of (\w+)')
+    WHEN CONTAINS_SUBSTR(region, "region") THEN REGEXP_EXTRACT(region, r'([\w\s-]+?)(?: border)?\sregion',1)
+    WHEN place = "1960 Great Chilean Earthquake (Valdivia Earthquake)" THEN "chile"
   ELSE
-  NULL
+  region
 END
-  AS state
-FROM 
-{{ ref('stg_kaggle_data') }}
+  AS region
+FROM (
+  SELECT
+    id,
+    time,
+    place,
+    type,
+    latitude,
+    longitude,
+    depth,
+    mag,
+    magType,
+    (
+      CASE
+        WHEN CONTAINS_SUBSTR(place, ",") THEN REGEXP_EXTRACT(LOWER(place), r',\s(.*?)$',1)
+      ELSE
+      LOWER(place)
+    END
+      ) AS region
+  FROM
+    {{ ref('stg_kaggle_data') }} )
 
 -- dbt build --select <model.sql> --var 'is_test_run: false'
 {% if var('is_test_run', default=true) %}
