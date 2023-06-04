674ee80d5d40c9031e7f292e3f6babb6842ee480
Hiroko Sakai <hiroko.hirobo@gmail.com>
2023-06-04 09:50:55 +0200

    prefect: bugfix for web_to_gcs_to_bq_daily

--- prefect/flows/web_to_gcs_to_bq_daily.py
+++ prefect/flows/web_to_gcs_to_bq_daily.py
@@ -6,10 +6,14 @@ from flows.utils.web_to_gcs_to_bq import web_to_gcs_to_bq
 
 @flow(name="world-earthquake-pipeline: web_to_gcs_to_bq_daily")
 def web_to_gcs_to_bq_daily() -> None:
-    yesterday = datetime.now() - timedelta(days=1)
+    today = datetime.now()
+    yesterday = today - timedelta(days=1)
     start_date = yesterday.date()
-    end_date = start_date
+    end_date = today
     web_to_gcs_to_bq(start_date,
                      end_date,
                      replace=False,
                      split_time=True)
+
+if __name__ == "__main__":
+    web_to_gcs_to_bq_daily()
