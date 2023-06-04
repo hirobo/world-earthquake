2429f49a72249353661913d91bbc7a2354c62470
Hiroko Sakai <hiroko.hirobo@gmail.com>
2023-06-04 23:39:27 +0200

    prefect: bugfix for web_to_gcs_to_bq_daily()

--- prefect/flows/web_to_gcs_to_bq_daily.py
+++ prefect/flows/web_to_gcs_to_bq_daily.py
@@ -9,7 +9,7 @@ def web_to_gcs_to_bq_daily() -> None:
     today = datetime.now()
     yesterday = today - timedelta(days=1)
     start_date = yesterday.date()
-    end_date = today
+    end_date = today.date()
     web_to_gcs_to_bq(start_date,
                      end_date,
                      replace=False,
