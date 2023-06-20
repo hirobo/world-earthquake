14ec5792d1acc413a442e2ff8981a83ed395d46f
Hiroko Sakai <hiroko.hirobo@gmail.com>
2023-06-20 13:49:46 +0200

    change to check last updated datetime on web_to_gsc_to_bq_daily

--- prefect/flows/web_to_gcs_to_bq_daily.py
+++ prefect/flows/web_to_gcs_to_bq_daily.py
@@ -1,19 +1,25 @@
-from datetime import datetime, timedelta
+from datetime import datetime
 from prefect import flow
 
 from flows.utils.web_to_gcs_to_bq import web_to_gcs_to_bq
+from flows.utils.gcs_to_bq import get_last_datetime
 
 
 @flow(name="world-earthquake-pipeline: web_to_gcs_to_bq_daily")
 def web_to_gcs_to_bq_daily() -> None:
-    today = datetime.now()
-    yesterday = today - timedelta(days=1)
-    start_date = yesterday.date()
-    end_date = today.date()
-    web_to_gcs_to_bq(start_date,
-                     end_date,
-                     replace=False,
-                     split_time=True)
+
+    # check the last date
+    last_datetime = get_last_datetime()
+    if last_datetime:
+        start_date = last_datetime.date()
+        end_date = datetime.now().date()
+        web_to_gcs_to_bq(start_date,
+                         end_date,
+                         replace=False,
+                         split_time=True)
+
+    else:
+        raise Exception("There is no data in BigQuery. Please run web_to_gcs_to_bq_all.")
 
 
 if __name__ == "__main__":
--- prefect/tests/test_web_to_gcs_to_bq_daily.py
+++ prefect/tests/test_web_to_gcs_to_bq_daily.py
@@ -1,21 +1,19 @@
 import unittest
 from unittest.mock import patch
-from datetime import datetime, timedelta
+from datetime import datetime, date
 from flows.web_to_gcs_to_bq_daily import web_to_gcs_to_bq_daily
 
 
 class TestWebToGcsToBqDaily(unittest.TestCase):
+    @patch("flows.web_to_gcs_to_bq_daily.get_last_datetime", return_value=datetime(2023, 6, 1, 10, 30))
     @patch("flows.web_to_gcs_to_bq_daily.web_to_gcs_to_bq")
-    def test_web_to_gcs_to_bq_daily(self, mock_web_to_gcs_to_bq):
+    def test_web_to_gcs_to_bq_daily(self, mock_web_to_gcs_to_bq, mock_get_last_datetime):
 
         web_to_gcs_to_bq_daily()
 
-        today = datetime.now()
-        yesterday = today - timedelta(days=1)
-
         mock_web_to_gcs_to_bq.assert_called_once_with(
-            yesterday.date(),
-            today.date(),
+            date(2023, 6, 1),
+            datetime.now().date(),
             replace=False,
             split_time=True
         )
